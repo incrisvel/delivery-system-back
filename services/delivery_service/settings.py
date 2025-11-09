8f9e069ecb69867de9fc5079fdd933f4dac000af
ThomasAugustoH <thomas.a.haskel@gmail.com>
2025-11-09 19:09:10 -0300

    :bug: fix: .env variables loading message

--- services/delivery_service/settings.py
+++ services/delivery_service/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent
 REPO_ROOT_DIR = BASE_DIR.parent.parent
 
 class Settings(BaseSettings):
-    print(f"Carregando variaveis de ambiente de: {REPO_ROOT_DIR / ".env", BASE_DIR / ".env"}")
+    print(f"Carregando variaveis de ambiente de: {REPO_ROOT_DIR / '.env', BASE_DIR / '.env'}")
 
     model_config = SettingsConfigDict(
         env_file=(REPO_ROOT_DIR / ".env", BASE_DIR / ".env"),
