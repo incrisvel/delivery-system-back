01bd5915b687bbc8c3be3e5f384110965f560135
ThomasAugustoH <thomas.a.haskel@gmail.com>
2025-11-21 18:52:37 -0300

    :sparkles: feat: Notification processing

---
+++ services/shared/notification.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from random import randint, choice
+from enum import Enum
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+
+from services.shared.simple_order import SimpleOrder
+
+class OrderStatus(str, Enum):
+    CONFIRMADO = "confirmed"
+    CRIADO = "created"
+    ATUALIZADO = "updated"
+    ATRIBUÍDO = "assigned"
+    EM_ENTREGA = "enroute"
+    ENTREGUE = "delivered"
+
+class Notification(BaseModel):
+    order: int
+    notifcation_time: datetime
+    status: OrderStatus
+
+    def __init__(self, order: SimpleOrder):
+        super().__init__(
+            order=order.order,
+            status=order.status,
+            notifcation_time=datetime.now()
+        )
+
+    def to_json(self):
+        return self.model_dump_json()
